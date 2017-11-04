17d46c5ae589b9652682a897a1ed8ce02d0066c4
goldenshieldlabs <33369201+goldenshieldlabs@users.noreply.github.com>
2017-11-04 15:27:45 -0500

    Add files via upload

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -7,7 +7,7 @@
 /***************** Configuration *****************/
 
 // Enter your email, where you want to receive the messages.
-$contact_email_to = "";
+$contact_email_to = "goldenshieldlabs@protonmail.com";
 
 // Subject prefix
 $contact_subject_prefix = "Contat Form Message: ";
